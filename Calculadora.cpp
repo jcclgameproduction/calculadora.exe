ade3ea16ce346cf4dbee70e69e7109442c7896b7
jcclgameproduction <61763550+jcclgameproduction@users.noreply.github.com>
2020-07-27 09:00:36 -0300

    Add files via upload

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <locale.h>
-#include <stdlib.h>
 #include <conio.h>
+#include <conio.c>
 //feito por João Carlos! 
 //Programa utilizado: Dev C++
 //1ª módulo desenvolvimento de sistemas Turma A
@@ -24,23 +24,55 @@ double pot(double a, double b){
 	return pow(a,b);
 }
 ///////////////////////////////////////
-
 /////////////void/////////////////////
-void ajuda() {
-	char ajuda;
-	std::cin >> ajuda;
-    switch (ajuda) {
-	case 'h':
-		system ("cls");
+
+void menu() {
+    char voltar;
+	char menu;
+	bool trag;
+	do	{
+	std::cout << "Bem vindo à calculadora!" << std::endl;
+    std::cout << "selecione uma das opções abaixo:\n" << std::endl;
+    std::cout << "[1]-Calculadora\n[2]-Instruções\n[3]-Créditos\n[4]-Sair" << std::endl;
+    
+	menu=getch();
+    if (menu=='1'){
+    	trag = true;
+    	system ("cls");
+	}
+	else
+	if (menu=='2'){
+	
+	system ("cls");
     std::cout << "Para usar a operação soma, digite +"<< std::endl;
     std::cout << "Para usar a operação subtração, digite -"<< std::endl;
     std::cout << "Para usar a operação divisão, digite /"<< std::endl;
     std::cout << "Para usar a operação multiplicação, digite *"<< std::endl;
-    std::cout << "Para usar a operação de potência, digite ^"<< std::endl << std::endl;
+    std::cout << "Para usar a operação de potência, digite ^\n\nAperte qualquer tecla para voltar ao Menu principal..."<< std::endl << std::endl;
+    voltar=getch();
+    trag= false;
+    system ("cls");
 }
-if (ajuda!= 'h'){
+else
+if (menu== '3'){
+	system ("cls");
+	std::cout <<"Programa Inteiramente feito por João Carlos!\n\nAperte qualquer tecla para voltar ao Menu principal..." << std::endl;
+	voltar=getch();
+	trag= false;
 	system ("cls");
 }
+else 
+if (menu== '4')	{
+	exit(0);
+}
+
+else {
+	system ("cls");
+	trag= false;
+	system ("cls");
+	
+}
+}while (trag==false);
 }
 ////////////////////////////////////
 
@@ -51,10 +83,10 @@ void calculadora () {
 	char operacao;
 	double numA, numB;
 	double resultados;
-	int f;
+	char f;
 	bool flag = true;
 	
-	while (flag) {
+	do{
 	
 	
 	std::cout << "Digite um Número:";
@@ -93,22 +125,17 @@ void calculadora () {
 		guarda = true;
 	std::cout << "O resultado é:" << resultados << std::endl;
 	}
-	std::cout << "\nVocê deseja continuar calculando?\n1-Sim\n2-Não" << std::endl;
-	std::cin >> f;
-	switch (f) {
-		case 1:
-			flag = true;
-			break;
-		case 2:
-			flag = false;
-			break;
-		default:
-			break;
+	std::cout << "\nVocê deseja continuar calculando?\ns-Sim\nn-Não" << std::endl; 
+	f=getch();
+	if (f=='s'){
+	flag = true;
+	system ("cls");
 	}
-	if (f == 1){
-		system ("cls");
+	else {
+		flag = false;
 	}
-}
+
+}while (flag);
 
 
 }
@@ -117,14 +144,28 @@ void calculadora () {
 
 
 int main(int argc, char **argv) {
+	char m;
+	bool crag;
 	
     setlocale(LC_ALL, "Portuguese");
+    textcolor (WHITE);
+    textbackground(BLUE);
 
-    std::cout << "Para ver o guia de usuário da calculadora, digite (h) e dê enter, se não digite qualquer coisa e dê enter " << std::endl;
-    
-	ajuda();
+do {
+system ("cls");
+	menu();
 
 	calculadora();
-
+	system("cls");
+	std::cout << "Você deseja voltar ao menu principal?\ns-Sim\nn-Não" << std::endl;
+	m=getch();
+	switch (m) {
+		case 's':
+			crag=true;
+			break;
+		default:
+			crag=false;
+	}
+}while(crag);
 	return 0;
 }
